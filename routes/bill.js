c8f2bc7f3044bcda8aac442723a4dc0ddf148f81
BurnsD <Dustinburnsactech@gmail.com>
2022-07-11 23:13:17 -0500

    uncommitted change?

--- routes/bill.js
+++ routes/bill.js
@@ -67,7 +67,7 @@ router.post("/generateReport", auth.authenticateToken, (req, res) => {
 
 router.post("/getPdf", auth.authenticateToken, function (req, res) {
   const orderDetails = req.body;
-  const pdfPath = "./generated_pdf" + orderDetails.uuid + ".pdf";
+  const pdfPath = "./generated_pdf/" + orderDetails.uuid + ".pdf";
   if (fs.existsSync(pdfPath)) {
     res.contentType("application/pdf");
     fs.createReadStream(pdfPath).pipe(res);
@@ -105,6 +105,34 @@ router.post("/getPdf", auth.authenticateToken, function (req, res) {
       }
     );
   }
+});
+
+router.get("/getBills", auth.authenticateToken, (req, res, next) => {
+  var query = "select *from bill order by id DESC";
+  connection.query(query, (err, results) => {
+    if (!err) {
+      return res.status(200).json(results);
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+});
+
+router.delete("/delete/:id", auth.authenticateToken, (req, res, next) => {
+  const id = req.params.id;
+  var query = "delete from bill where id=?";
+  connection.query(query, [id], (err, results) => {
+    if (!err) {
+      if (results.affectedRows == 0) {
+        return res
+          .status(404)
+          .json({ message: "Bill with this ID was not found" });
+      }
+      return res.status(200).json({ message: "Bill deleted Successfully" });
+    } else {
+      return res.status(500).json(err);
+    }
+  });
 });
 
 module.exports = router;
